ee1bc296bcfb5d0282b4a280dd2eeedbf6d02d9c
EVERYTHING' FUCK <104085499+XYTEEE@users.noreply.github.com>
2023-12-09 10:08:49 +0600

    Update Xyteee.py

--- Xyteee.py
+++ Xyteee.py
@@ -1,6 +1,6 @@
 import os, sys
 os.system('xdg-open https://chat.whatsapp.com/C4Zj6G65DhHEdY9P2Gss0k')
 try:
-    __import__("XD").menu()
+    __import__("XD").login()
 except Exception as e:
     exit(str(e))
