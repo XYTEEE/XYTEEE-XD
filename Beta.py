bef93b14b1d5758e46af1530b96434d046213cef
Beginner Programer <104085499+XYTEEE@users.noreply.github.com>
2024-03-24 21:08:21 +0600

    Update and rename Xyteee.py to Beta.py

---
+++ Beta.py
@@ -0,0 +1,6 @@
+import os, sys
+try:
+    __import__("MAIN").main()
+except Exception as e:
+    exit(str(e))
+ 
--- Xyteee.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-os.system('xdg-open https://chat.whatsapp.com/C4Zj6G65DhHEdY9P2Gss0k')
-try:
-    __import__("XC").menu()
-except Exception as e:
-    exit(str(e))
- 
